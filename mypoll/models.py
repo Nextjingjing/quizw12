73d2e344ee2842e00c9c2af5f9950c16f7269833
SNext <snext2004@gmail.com>
2025-03-13 14:21:38 +0700

    add private feature

--- mypoll/models.py
+++ mypoll/models.py
@@ -11,6 +11,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     rate = models.IntegerField(default=0)
+    is_private = models.BooleanField(default=False)
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
--- mypoll/views.py
+++ mypoll/views.py
@@ -9,6 +9,19 @@ from django.utils import timezone
 
 from .models import Choice, Question
 
+class PrivateView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now(), is_private = True).order_by("-pub_date")[
+        :5
+    ]
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
@@ -19,7 +32,7 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        return Question.objects.filter(pub_date__lte=timezone.now(), is_private = False).order_by("-pub_date")[
         :5
     ]
 
